3645d8f4b0a5e35c0b063ba56f21d4232da918e0
Muhammad Waqar <waqarilyas.dev@gmail.com>
2024-11-05 16:21:12 +0500

    Feat: Update layout and social links for RTL support and localization

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,92 @@
 "use client";
 import Image from "next/image";
 import { BiLocationPlus } from "react-icons/bi";
-import {
-  FaEnvelope,
-  FaExternalLinkAlt,
-  FaFacebook,
-  FaInstagram,
-  FaLinkedin,
-  FaPhone,
-  FaTiktok,
-  FaTwitter,
-  FaWhatsapp,
-} from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { FaExternalLinkAlt, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { LuPhone } from "react-icons/lu";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiTiktokLine } from "react-icons/ri";
+import { SlSocialLinkedin } from "react-icons/sl";
 
 const SocialLinksPage = () => {
   const links = [
-    // {
-    //   title: "Website",
-    //   url: "http://www.yadnxd.com",
-    //   icon: FaGlobe,
-    //   description: "Discover more about our services and projects",
-    //   bgColor: "bg-white",
-    //   hoverColor: "hover:bg-gray-50",
-    // },
-
     {
-      title: "Twitter",
+      title: "تويتر",
       url: "https://x.com/yadnxd?t=xESgy0TMXyGCJivzdbRQ0A&s=09",
-      icon: FaTwitter,
-      description: "Follow us for the latest updates and news",
+      icon: FaXTwitter,
+      description: "تابعنا لأحدث الأخبار والتحديثات",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "LinkedIn",
+      title: "لينكد إن",
       url: "https://www.linkedin.com/company/yadnxd/",
-      icon: FaLinkedin,
-      description: "Connect with us professionally",
+      icon: SlSocialLinkedin,
+      description: "تواصل معنا مهنياً",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "Instagram",
+      title: "إنستغرام",
       url: "https://www.instagram.com/yadnxd?igsh=Y2xpYzFncWVwdzY5",
       icon: FaInstagram,
-      description: "Get a glimpse behind the scenes of YADN",
+      description: "اكتشف الجانب الآخر من YADN",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "Facebook",
+      title: "فيسبوك",
       url: "https://www.facebook.com/profile.php?id=61551582941267&mibextid=ZbWKwL",
-      icon: FaFacebook,
-      description: "Follow us on Facebook",
+      icon: CiFacebook,
+      description: "تابعنا على فيسبوك",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "TikTok",
+      title: "تيك توك",
       url: "https://www.tiktok.com/@yadnxd",
-      icon: FaTiktok,
-      description: "Watch our latest TikTok videos",
+      icon: RiTiktokLine,
+      description: "شاهد آخر فيديوهاتنا على تيك توك",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "Mail",
+      title: "البريد الإلكتروني",
       url: "mailto:info@yadnxd.com",
-      icon: FaEnvelope,
-      description: "Contact us via email",
+      icon: MdOutlineMailOutline,
+      description: "تواصل معنا عبر البريد الإلكتروني",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "WhatsApp",
+      title: "واتساب",
       url: "https://wa.me/+966550513539",
-      icon: FaWhatsapp, // Use an appropriate icon for WhatsApp if desired
-      description: "Chat with us on WhatsApp",
+      icon: FaWhatsapp,
+      description: "تحدث معنا عبر واتساب",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "Phone",
+      title: "الهاتف",
       url: "tel:+966550513539",
-      icon: FaPhone,
-      description: "Call us",
+      icon: LuPhone,
+      description: "اتصل بنا",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
     {
-      title: "Location",
+      title: "الموقع",
       url: "https://maps.app.goo.gl/ZMZZ8cKnNqEyAyJ66",
       icon: BiLocationPlus,
-      description: "Find us here",
+      description: "اعثر علينا هنا",
       bgColor: "bg-white",
       hoverColor: "hover:bg-gray-50",
     },
   ];
 
   return (
-    <div className="min-h-screen flex flex-col justify-between bg-[#F3F3F3] px-4">
+    <div className="min-h-screen flex flex-col justify-between bg-[#F3F3F3] px-4 rtl">
       <div className="max-w-2xl w-full mx-auto py-8 flex flex-col items-center">
         {/* Profile Section */}
         <div className="flex flex-col items-center mb-6">
@@ -124,9 +111,9 @@ const SocialLinksPage = () => {
                   transition-all duration-300 shadow-sm hover:shadow-md border border-gray-100
                   transform hover:-translate-y-1`}
               >
-                <div className="flex items-center space-x-4">
+                <div className="flex items-center space-x-reverse space-x-4">
                   <Icon className="w-6 h-6 text-[#ED1E78] group-hover:text-gray-800 transition-colors" />
-                  <div>
+                  <div className="text-right">
                     <h2 className="text-lg font-semibold text-gray-800">
                       {link.title}
                     </h2>
@@ -146,9 +133,9 @@ const SocialLinksPage = () => {
       <footer className="text-center text-gray-500 py-4">
         <p className="text-sm">
           © {new Date().getFullYear()}{" "}
-          <span className="font-medium text-gray-600">YADN</span>
+          <span className="font-medium text-gray-600">یدن</span>
           {" • "}
-          <span className="text-gray-400">Innovating the Future</span>
+          <span className="text-gray-400">ابتكار المستقبل</span>
         </p>
       </footer>
     </div>
