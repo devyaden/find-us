28c738aa84956f8ca1c18f99503ec81937bf8348
devyaden <149952224+devyaden@users.noreply.github.com>
2024-11-11 11:10:30 +0000

    opengraph-image.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,24 +19,23 @@ export const metadata: Metadata = {
   keywords:
     "Yadn, Al-Safar Group, business solutions, policy governance, procedure governance, service design, knowledge management, digitization, digital transformation, company growth, administrative structure, corporate development, business consulting, efficient growth, business innovation, management solutions, business policy, service management, digital services, يدن, مجموعة الصفر, حلول الأعمال, حوكمة السياسات, حوكمة الإجراءات, تصميم الخدمات, إدارة المعرفة, الرقمنة, التحول الرقمي, نمو الشركات, الهيكل الإداري, تطوير الشركات, استشارات الأعمال, النمو الفعال, الابتكار في الأعمال, حلول الإدارة, سياسات الشركات, إدارة الخدمات, الخدمات الرقمية",
   openGraph: {
-    title: "تابع يدن على وسائل التواصل الاجتماعي",
-    description:
-      "Yadn was established in 2022 as a subsidiary of Al-Safar Group. It provides business solutions tailored to company needs and administrative structures in four key areas: policy and procedure governance, service design, knowledge management, and digitization, enabling efficient development and growth. | تأسست يدن عام 2022 تابعة لمجموعة الصفر وهي شركة رائدة في توفير حلول لأعمالك بما يتناسب مع الاحتياجات والبنية الإدارية للشركة، وتقديم الحلول في مجالات أربعة أساسية وهي: حوكمة السياسات والإجراءات، تصميم الخدمات، إدارة المعرفة، والرقمنة للشركات حتى تتمكن من التطور والنمو بكفاءة",
+    title: "يدن حاضرة على جميع منصات التواصل الاجتماعي",
+    description: "ستجد هنا روابط مباشرة لجميع حساباتنا",
     url: "https://yadnxd.com",
     images: [
       {
-        url: "../public/111.png",
+        url: "./opengraph-image.png",
         alt: "YADN Logo",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "تابع يدن على وسائل التواصل الاجتماعي",
+    title: "يدن حاضرة على جميع منصات التواصل الاجتماعي",
     description: "With Yadn you will reach the highest levels of efficiency. | مع يدن ستصل الى أعلى مستويات الكفاءة. ",
     images: [
       {
-        url: "../public/111.png",
+        url: "./opengraph-image.png",
         alt: "YADN Logo",
       },
     ],
